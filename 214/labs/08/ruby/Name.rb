47e00fcdf61061b028168afa51de8ff987c911a6
Chan Kim <kimchan04@gmail.com>
2018-04-04 00:09:57 -0400

    hello

--- 214/labs/08/ruby/Name.rb
+++ 214/labs/08/ruby/Name.rb
@@ -7,20 +7,21 @@
 
 module Names
 
-class Name
+    class Name
 
-  def initialize(first, middle, last)
-    @first, @middle, @last = first, middle, last
-  end
+        def initialize(first, middle, last)
+            @first, @middle, @last = first, middle, last
+        end
 
-  attr_reader :first, :middle, :last
+        attr_reader :first, :middle, :last
 
-  def fullName
-    @first + " " + @middle + " " + @last
-  end
+        def fullName
+            @first + " " + @middle + " " + @last
+        end
 
-  def print
-    puts fullName
-    fullName
-  end
+        def print
+            puts fullName
+            fullName
+        end
+    end
 end
