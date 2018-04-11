9b51163e28b72689031e4f4d3df87d0ef4e7f185
Chan Kim <kimchan04@gmail.com>
2018-04-10 22:58:22 -0400

    hello

--- 232/homework06/bakeshop.c
+++ 232/homework06/bakeshop.c
@@ -1,3 +1,8 @@
+/* bakeshop.c for CS 232 at Calvin College
+ * Professor: Dr. Victor Norman
+ * Student: Chan Kim (ck45)
+ * Date: 4/10/2018
+ *********************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,10 +21,11 @@ sem_t Store;
 sem_t CheckOut;
 sem_t Receipt;
 
-/*
- * bakeBread()
- * 
- */
+/************************************************
+ * bakeBread() represent Baker's characteristics
+ * PRE: Baker semaphore has been declared.
+ * Return: Run until breadsCount becomes 10.
+ ************************************************/
 
 void *bakeBread()
 {
@@ -30,11 +36,16 @@ void *bakeBread()
         breadsCount++;
         fprintf(stderr, "A loaf of bread is completed and available for sale\n");
         sem_post(&Baker);
-        // introduce one second delays
         usleep(1000000);
     }
     fprintf(stderr, "All the baking is done\n");
 }
+
+/***********************************************************************
+ * cashRegister() represents Baker's characteristics as a cash register
+ * PRE: Baker and Receipt semaphores have been declared.
+ * Return: Run until customers becomes 10, and while customersCheck > 0.
+ ***********************************************************************/
 
 void *cashRegister()
 {
@@ -46,32 +57,36 @@ void *cashRegister()
             
             sem_wait(&Receipt);
             fprintf(stderr, "The baker prints the customer a receipt\n");
-            usleep(1000000);            
-                
+            usleep(1000000);                 
             sem_post(&Receipt);
+
             fprintf(stderr, "The baker hands the customer a receipt\n");
             customers++;
             customersCheck--;
-            
             sem_post(&Baker);
         }
  
     }
 }
 
+/*****************************************************************************
+ * customer(id) represents customer characteristics.
+ * PRE: Store, Customer, CheckOut, and CheckOut semaphores have been declared.
+ *****************************************************************************/
+
 void *customer(void *id)
 {
     sem_wait(&Store);
     fprintf(stderr, "Customer %ld has entered the store\n", (long)id);
+
     sem_wait(&Customer);
     while (breads == 0)
     {
         usleep(1000000);
     }
-
     breads--;
-    
     sem_post(&Customer);
+
     fprintf(stderr, "Customer %ld obtains a load of bread\n", (long)id);
     usleep(1000000);
 
@@ -81,17 +96,24 @@ void *customer(void *id)
     usleep(1000000);
     
     sem_wait(&Receipt);
-
     sem_post(&Receipt);
+
     fprintf(stderr, "Customer %ld gets the receipt\n", (long)id);
     sem_post(&CheckOut);
 
     sem_wait(&Customer);
+
     fprintf(stderr, "Customer %ld leaves the store\n", (long)id); 
     sem_post(&Customer);
        
     sem_post(&Store);
 }
+
+/*******************************************
+ * main() runs the program.
+ *        Semaphores have been initialized.
+ *        Threads have been created.
+ *******************************************/
 
 int main()
 {
